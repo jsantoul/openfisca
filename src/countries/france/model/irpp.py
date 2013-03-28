5690bf1b1753da5980a631aead51f86bd1f388e7
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2013-03-28 18:02:00 +0000

    correction in _glo

--- src/countries/france/model/irpp.py
+++ src/countries/france/model/irpp.py
@@ -475,12 +475,13 @@ def _rfr(rni, alloc, f3va, f3vg, f3vi, rfr_cd, rfr_rvcm, rpns_exon, rpns_pvce, r
     '''
     return max_(0, rni - alloc) + rfr_cd + rfr_rvcm + rev_cap_lib + f3vi + rpns_exon + rpns_pvce + f3va + f3vg
  
-def _glo(f1tv, f1tw, f1tx, f1uv, f1uw, f1ux, f3vf, f3vi, f3vj, f3vk):
+def _glo(f1tv, f1tw, f1tx, f3vf, f3vi, f3vj, f3vk): 
+    # TODO: f1uv, f1uw, f1ux deletion to check
     '''
     Gains de levée d'option
     'foy'
     '''
-    return f1tv + f1tw + f1tx + f1uv + f1uw + f1ux + f3vf + f3vi + f3vj + f3vk                   
+    return f1tv + f1tw + f1tx  + f3vf + f3vi + f3vj + f3vk   # + f1uv + f1uw + f1ux                
 
 def _rev_cap_bar(f2dc, f2gr, f2ch, f2ts, f2go, f2tr, f2fu, avf, f2da, f2ee, _P):
     '''

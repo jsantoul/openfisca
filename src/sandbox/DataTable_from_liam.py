11078ba0dd9c00171a6a91927dc126933566a3fe
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2013-03-28 18:03:52 +0000

    sandox

--- src/sandbox/DataTable_from_liam.py
+++ src/sandbox/DataTable_from_liam.py
@@ -8,11 +8,13 @@ import numpy as np
 import pdb
 
 input = "M:\Myliam2\Model\simulTest.h5"
-input = "C:\Myliam2\Model\simulTest.h5"
-output = "C:\Myliam2\Model\to_OF.h5"
+input = "T:\Myliam2\Model\simulTest.h5"
+output = "T:\Myliam2\Model\to_OF.h5"
+
+name_convertion = {'person':'ind','declar':'foy','menage':'men'}
 
 store = HDFStore(input)
-goal = HDFStore("C:/openfisca/src/countries/france/data/survey.h5")
+goal = HDFStore("T:/openfisca/src/countries/france/data/survey.h5")
 #available_years = sorted([int(x[-4:]) for x in  store.keys()])
 base = 'entities/person'
 table = store[str(base)]
@@ -28,24 +30,26 @@ for year in years:
         table_nom = store[str(base)] 
         to_keep = table_nom['period']==year
         table_year[nom] = table_nom[to_keep]
-        key = 'survey_'+str(year) + '/'+str(nom)
+        nom_of = name_convertion[nom]
+        key = 'survey_'+str(year) + '/'+str(nom_of)
         store.put(key, table_year[nom])
-pdb.set_trace()
-store.keys()
-goal.keys()
-store.select('survey_2009/declar')
-goal.select('survey_2009')
-store.close()
-
-
-
-
-
-tab = table_nom_year[2009]
-tabp = tab['person']
-tabp_ref = tabp['quires']==1
-tabp_res = tabp['res']
-tabp_res[tabp_ref]
+        
+#pdb.set_trace()
+#store.keys()
+#goal.keys()
+#store.select('survey_2009/declar')
+#goal.select('survey_2009')
+#store.close()
+#
+#
+#
+#
+#
+#tab = table_nom_year[2009]
+#tabp = tab['person']
+#tabp_ref = tabp['quires']==1
+#tabp_res = tabp['res']
+#tabp_res[tabp_ref]
 
 
     

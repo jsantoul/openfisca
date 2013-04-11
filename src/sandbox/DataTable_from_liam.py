771fac835bbd9186219038baa287e1155190a774
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2013-04-12 00:28:50 +0200

    look for an issue in liam

--- src/sandbox/DataTable_from_liam.py
+++ src/sandbox/DataTable_from_liam.py
@@ -10,13 +10,13 @@ import time
 from src.lib.simulation import SurveySimulation 
 
 input = "M:\Myliam2\Model\simulTest.h5"
-input = "C:\Myliam2\Model\simulTest.h5"
+input = "M:\Myliam2\Model\simulTest.h5"
 output = "C:\Myliam2\Model\to_OF.h5"
 
 name_convertion = {'person':'ind','declar':'foy','menage':'men', 'fam':'fam'}
 
 store = HDFStore(input)
-goal = HDFStore("C:/openfisca/src/countries/france/data/surveyLiam.h5")
+goal = HDFStore("M:/openfisca/src/countries/france/data/surveyLiam.h5")
 #available_years = sorted([int(x[-4:]) for x in  store.keys()])
 
 
@@ -32,8 +32,7 @@ table[ent] = store[str(base)]
 years = np.unique(table[ent]['period'].values)
 # rename variables to make them OF ones
 table[ent] = table[ent].rename(columns={'res': 'idmen', 'quires': 'quimen', 'foy': 'idfoy'})
-table[ent]['quimen'] = table[ent]['quimen'] - 1
-table[ent]['quifoy'] = table[ent]['quifoy'] - 1
+
 # create fam base
 table[ent][['idfam','quifam']] = table[ent][['idmen','quimen']]
 # save information on qui == 0
@@ -51,6 +50,13 @@ for nom in ('menage','declar','fam'):
         table[ent] = store[str(base)].rename(columns={'id': ident})
         table[ent] = merge(table[ent], eval(ent +'0'), how='left', left_on=[ident,'period'], right_on=[ident,'period'])
                    
+# test sur le nombre de qui ==0
+for year in [2010]:
+    for ent in ('men','foy','fam'):
+        tab = table[ent].ix[table[ent]['period']==year,['id','id'+ent,'idfam']]
+        ind = table['ind'].ix[table['ind']['period']==year,['qui'+ent]] 
+        list_ind =  ind[ind==0]
+        pdb.set_trace()   
             
 for year in years:
     goal.remove('survey_'+str(year))
@@ -78,7 +84,7 @@ for year in years:
     simu = SurveySimulation()
     simu.set_config(year = yr, country = country)
     simu.set_param()
-    simu.set_survey(filename="C:/openfisca/src/countries/france/data/surveyLiam.h5", num_table=3)
+    simu.set_survey(filename="M:/openfisca/src/countries/france/data/surveyLiam.h5", num_table=3)
     simu.compute()
     fin3  = time.clock()
 
